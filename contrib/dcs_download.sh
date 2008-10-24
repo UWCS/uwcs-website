02205e7ba6d3b4bade418042835b300fccc18a8a
Daniel Watkins <daniel@daniel-watkins.co.uk>
2008-10-24 19:14:03 +0000

    Added python-markdown to DCS installation script.

--- contrib/dcs_download.sh
+++ contrib/dcs_download.sh
@@ -2,11 +2,14 @@
 
 set -eu
 
-cd /var/tmp
-mkdir website
-cd website
+WEBSITE_DIR=/var/tmp/website
+
+mkdir -p $WEBSITE_DIR
+cd $WEBSITE_DIR
 
 curl http://mulletron.uwcs.co.uk/django.tar | tar xv
 curl http://mulletron.uwcs.co.uk/python26.tar | tar xv
+curl http://ftp.de.debian.org/debian/pool/main/p/python-markdown/python-markdown_1.7.orig.tar.gz | tar xv && cd markdown-1.7 && python setup.py install --prefix=/var/tmp/website
+cd $WEBSITE_DIR
 
-export PYTHONPATH=/var/tmp/website/lib/python2.4/site-packages/:$PYTHONPATH
+export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
