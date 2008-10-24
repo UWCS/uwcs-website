096633fc6bcad5e433cd709913ed992eea02a698
Daniel Watkins <daniel@daniel-watkins.co.uk>
2008-10-24 21:54:44 +0000

    Everything's a function!

--- contrib/dcs_download.sh
+++ contrib/dcs_download.sh
@@ -2,12 +2,6 @@
 
 set -u
 
-WEBSITE_DIR=/var/tmp/website
-export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
-
-mkdir -p $WEBSITE_DIR
-cd $WEBSITE_DIR
-
 standard_setup ()
 {
     # $1 = source URL
@@ -16,9 +10,34 @@ standard_setup ()
     cd $WEBSITE_DIR
 }
 
-curl http://mulletron.uwcs.co.uk/django.tar | tar xv
-curl http://mulletron.uwcs.co.uk/python26.tar | tar xv
-standard_setup http://pypi.python.org/packages/source/M/Markdown/markdown-1.7.tar.gz markdown-1.7
-wget http://docutils.sourceforge.net/docutils-snapshot.tgz && tar zxvf docutils-snapshot.tgz && cd docutils && python setup.py install --prefix=/var/tmp/website
+install_django ()
+{
+    curl http://mulletron.uwcs.co.uk/django.tar | tar xv
+}
+
+install_python ()
+{
+    curl http://mulletron.uwcs.co.uk/python26.tar | tar xv
+}
+
+install_markdown ()
+{
+    standard_setup http://pypi.python.org/packages/source/M/Markdown/markdown-1.7.tar.gz markdown-1.7
+}
+
+install_docutils ()
+{
+    wget http://docutils.sourceforge.net/docutils-snapshot.tgz && tar zxvf docutils-snapshot.tgz && cd docutils && python setup.py install --prefix=/var/tmp/website
+    cd $WEBSITE_DIR
+}
+
+WEBSITE_DIR=/var/tmp/website
+export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
+
+mkdir -p $WEBSITE_DIR
 cd $WEBSITE_DIR
 
+install_python
+install_django
+install_markdown
+install_docutils
