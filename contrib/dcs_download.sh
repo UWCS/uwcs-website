dc4f2d09a67b1db78f34fd5f966d9a11500507fb
Daniel Watkins <daniel@daniel-watkins.co.uk>
2008-10-24 19:16:58 +0000

    MONKEYS_AT

--- contrib/dcs_download.sh
+++ contrib/dcs_download.sh
@@ -9,7 +9,7 @@ cd $WEBSITE_DIR
 
 curl http://mulletron.uwcs.co.uk/django.tar | tar xv
 curl http://mulletron.uwcs.co.uk/python26.tar | tar xv
-curl http://ftp.de.debian.org/debian/pool/main/p/python-markdown/python-markdown_1.7.orig.tar.gz | tar xv && cd markdown-1.7 && python setup.py install --prefix=/var/tmp/website
+curl http://ftp.de.debian.org/debian/pool/main/p/python-markdown/python-markdown_1.7.orig.tar.gz | tar zxv && cd markdown-1.7 && python setup.py install --prefix=/var/tmp/website
 cd $WEBSITE_DIR
 
 export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
