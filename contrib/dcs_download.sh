1e7be36cf86717ab7c86a84e8aaf754384021bc4
Daniel Watkins <daniel@daniel-watkins.co.uk>
2008-10-24 19:39:22 +0000

    Extracted standard Python installation spiel.

--- contrib/dcs_download.sh
+++ contrib/dcs_download.sh
@@ -3,15 +3,22 @@
 set -u
 
 WEBSITE_DIR=/var/tmp/website
+export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
 
 mkdir -p $WEBSITE_DIR
 cd $WEBSITE_DIR
 
+standard_setup ()
+{
+    # $1 = source URL
+    # $2 = untarred directory
+    curl $1 | tar zxv && cd $2 && python setup.py install --prefix=/var/tmp/website
+    cd $WEBSITE_DIR
+}
+
 curl http://mulletron.uwcs.co.uk/django.tar | tar xv
 curl http://mulletron.uwcs.co.uk/python26.tar | tar xv
-curl http://ftp.de.debian.org/debian/pool/main/p/python-markdown/python-markdown_1.7.orig.tar.gz | tar zxv && cd markdown-1.7 && python setup.py install --prefix=/var/tmp/website
-cd $WEBSITE_DIR
+standard_setup http://pypi.python.org/packages/source/M/Markdown/markdown-1.7.tar.gz markdown-1.7
 wget http://docutils.sourceforge.net/docutils-snapshot.tgz && tar zxvf docutils-snapshot.tgz && cd docutils && python setup.py install --prefix=/var/tmp/website
 cd $WEBSITE_DIR
 
-export PYTHONPATH=$WEBSITE_DIR/lib/python2.4/site-packages/:$PYTHONPATH
