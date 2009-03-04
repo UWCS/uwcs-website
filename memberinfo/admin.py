f84b59f1e0e072f36304778c0bbf3f23e266400b
Tim Monks <t.r.monks@gmail.com>
2009-03-04 03:18:48 +0000

    attempt to inline extra user metadata

--- memberinfo/admin.py
+++ memberinfo/admin.py
@@ -1,16 +1,47 @@
 from compsoc.memberinfo.models import *
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 class MemberJoinAdmin(admin.ModelAdmin):
     list_filter = ['year']
 
-admin.site.register(Member)
-admin.site.register(WebsiteDetails)
-admin.site.register(NicknameDetails)
+class MemberInline(admin.StackedInline):
+    model = Member
+
+class WebsiteDetailsInline(admin.StackedInline):
+    model = WebsiteDetails
+
+class NicknameDetailsInline(admin.StackedInline):
+    model = NicknameDetails
+
+class ShellAccountInline(admin.StackedInline):
+    model = ShellAccount
+
+class DatabaseAccountInline(admin.StackedInline):
+    model = DatabaseAccount
+
+class QuotaInline(admin.TabularInline):
+    model = Quota
+
+class MyUserAdmin(UserAdmin):
+    inlines = [MemberInline,
+               NicknameDetailsInline,
+               WebsiteDetailsInline,
+               ShellAccountInline,
+               DatabaseAccountInline,
+               QuotaInline,]
+
+# re-register useradmin
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
+
+#admin.site.register(Member)
+#admin.site.register(WebsiteDetails)
+#admin.site.register(NicknameDetails)
 admin.site.register(MemberJoin,MemberJoinAdmin)
-admin.site.register(ShellAccount)
-admin.site.register(DatabaseAccount)
-admin.site.register(Quota)
+#admin.site.register(ShellAccount)
+#admin.site.register(DatabaseAccount)
+#admin.site.register(Quota)
 admin.site.register(MailingList)
-admin.site.register(Term)
+#admin.site.register(Term)
 
