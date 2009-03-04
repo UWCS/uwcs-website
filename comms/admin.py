a2319a167ff2052ec273e88580d0555287d5b2bd
Tim Monks <t.r.monks@gmail.com>
2009-03-04 03:10:21 +0000

    actual improvements to comms admin

--- comms/admin.py
+++ comms/admin.py
@@ -2,8 +2,9 @@ from compsoc.comms.models import Communication
 from django.contrib import admin
 
 class CommunicationAdmin(admin.ModelAdmin):
-    fields = ('type', 'date', 'text')
-    list_display = ('type', 'title')
+    fields = ('type', 'title', 'date', 'text')
+    list_display = ('title', 'type')
+    list_filter = ('type',)
 
 admin.site.register(Communication, CommunicationAdmin)
 
