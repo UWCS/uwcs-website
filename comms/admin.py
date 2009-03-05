6b4d18d53d24ddaf33e7942ef8e2a3f8a2de0300
Connor Dunn <connorhd@gmail.com>
2009-03-05 01:29:11 +0000

    Added and ordered by date on admin communications page.

--- comms/admin.py
+++ comms/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 class CommunicationAdmin(admin.ModelAdmin):
     fields = ('type', 'title', 'date', 'text')
-    list_display = ('title', 'type')
+    list_display = ('title', 'type', 'date')
     list_filter = ('type',)
+    ordering = ('-date',)
 
 admin.site.register(Communication, CommunicationAdmin)
-
