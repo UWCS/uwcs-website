adac2e0e4e4738058e81f54451808e66c89b4b4e
Richard Warburton <richard.warburton@gmail.com>
2009-03-09 13:52:44 +0000

    committing forms

---
+++ memberinfo/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class DatabaseForm(forms.Form):
+    name = forms.CharField()
+
+class ShellForm(forms.Form):
+    name = forms.CharField()
+
+class QuotaForm(forms.Form):
+    quota = forms.IntegerField(label='Units (x500MB) quota:')
+
+class NicknameForm(forms.Form):
+    name = forms.CharField(required=False)
+
+class WebsiteForm(forms.Form):
+    url = forms.URLField()
+    title = forms.CharField()
+
+class PublishForm(forms.Form):
+    publish = forms.BooleanField(label='Publish Details',required=False)
+    
