7bb3af22acef81782f40a15d6b0bfa8e85c32249
Richard Warburton <richard.warburton@gmail.com>
2009-04-21 00:16:26 +0000

    fixes bug in populate script

--- memberinfo/management/commands/populate.py
+++ memberinfo/management/commands/populate.py
@@ -3,7 +3,7 @@ from django.core.management.base import NoArgsCommand
 from django.contrib.auth.models import User
 from compsoc.memberinfo.models import *
 from compsoc.events.models import *
-from datetime import datetime
+from datetime import date
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list
@@ -20,23 +20,25 @@ class Command(NoArgsCommand):
             syncdb has been run, and there is a user
         '''
         u = User.objects.all()[0]
-        mem = Member(user=u,showDetails=True,guest=False)
-        mem.save()
+        try:
+            mem = Member(user=u,showDetails=True,guest=False)
+            mem.save()
+        except: pass #user already has member object 
        
         lists = ['announce','discuss','exec','gaming','jobs','socials','techteam']
         for suffix in lists:
             u.mailinglist_set.create(list='compsoc-'+suffix+'@uwcs.co.uk')
 
         terms = [
-            ('AU',1,datetime(2008,9,29)),
-            ('SP',11,datetime(2009,1,5)),
-            ('SU',21,datetime(2009,4,20)),
-            ('AU',1,datetime(2007,10,01)),
-            ('SP',11,datetime(2008,1,07)),
-            ('SU',21,datetime(2008,4,21)),
+            ('AU',1,date(2008,9,29)),
+            ('SP',11,date(2009,1,5)),
+            ('SU',21,date(2009,4,20)),
+            ('AU',1,date(2007,10,01)),
+            ('SP',11,date(2008,1,07)),
+            ('SU',21,date(2008,4,21)),
         ]
-        for (t,num,date) in terms:
-            term = Term(start_date=date,start_number=num,length=10,which=t)
+        for (t,num,d) in terms:
+            term = Term(start_date=d,start_number=num,length=10,which=t)
             term.save()
 
         for yr in range(2001,2009):
