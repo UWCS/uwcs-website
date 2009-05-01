54282570abdce58c2640d714e94f098583fb3404
Richard Warburton <richard.warburton@gmail.com>
2009-05-01 14:45:39 +0000

    written a simple similarity decider

---
+++ events/similarity.py
@@ -0,0 +1,73 @@
+import math
+
+from compsoc.events.models import *
+from collections import defaultdict
+from django.contrib.auth.models import User
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = float(x)
+        self.y = float(y)
+        
+    def __getitem__(self, key):
+        if( key == 0):
+            return self.x
+        elif( key == 1):
+            return self.y
+        else:
+            raise Exception("Invalid key to Point")
+        
+    def __setitem__(self, key, value):
+        if( key == 0):
+            self.x = value
+        elif( key == 1):
+            self.y = value
+        else:
+            raise Exception("Invalid key to Point")
+        
+    def __unicode__(self):
+        return u'(%s,%s)' % (self.x,self.y)
+    
+    def distance(self, point2):
+        """Returns the distance between to another point"""
+        return math.sqrt( ( (self[0]-point2[0])**2 + (self[1]-point2[1])**2) )
+        
+def ave(points):
+    return reduce(lambda x,y:x+y,points)/len(points)
+
+def distance_matrix():
+    """
+        Calculates a mapping from users onto distances
+    """
+    # Build lookup map
+    # TODO: remove duplicate computations
+    lookup = defaultdict(lambda: defaultdict(lambda: []))
+    for event in Event.objects.all():
+        seating = SeatingRevision.objects.filter(event=event).order_by('-number')
+        if seating:
+            latest = seating[0]
+            cache = {}
+            for s in latest.seating_set.all():
+                cache[s.user_id] = Point(s.row,s.col)
+            for user in cache.keys():
+                for other in cache.keys():
+                    if other != user:
+                        lookup[user][other].append(cache[user].distance(cache[other]))
+    
+    results = defaultdict(lambda:{})
+    # Average distances
+    # TODO: weight distances
+    for user,others in lookup.items():
+        for other,values in others.items():
+            results[user][other] = ave(values)
+
+    return results
+
+def closest_person():
+    distances = distance_matrix()
+    results = []
+    for user,others in distances.items():
+        other,score = sorted(others.items(),key=lambda (u,v):v)[0]
+        results.append((User.objects.get(pk=user),User.objects.get(pk=other),score))
+    return results
+
