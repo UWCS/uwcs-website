392409f82f5c2b43776bb52b585662acb7b93b6b
Richard Warburton <richard.warburton@gmail.com>
2009-05-01 00:09:10 +0000

    fixed last commit

--- search/__init__.py
+++ search/__init__.py
@@ -3,6 +3,8 @@ registry = {}
 def register(model, fields, order='pk', filter=False, results=5):
     registry[str(model)] = (model, fields, results, order, filter)
 
+class LoopBreak(Exception): pass
+
 def search_for_string(search_string):
     search_string = search_string.lower()
     matches = []
@@ -16,19 +18,21 @@ def search_for_string(search_string):
         else:
             objects = model.objects.all()
         counter = 0
-        for object in objects.order_by(order):
-            for field in fields:
-                try:
-                    searchee = getattr(object, field)
-                except AttributeError:
-                    pass
-                if callable(searchee):
-                    searchee = searchee()
-                if search_string in searchee.lower():
-                    matches.append(object)
-                    counter += 1
-                    if counter >= results:
-                        break
-                        
-
+        try:
+            for object in objects.order_by(order):
+                for field in fields:
+                    try:
+                        searchee = getattr(object, field)
+                    except AttributeError:
+                        pass
+                    if callable(searchee):
+                        searchee = searchee()
+                    if search_string in searchee.lower():
+                        matches.append(object)
+                        counter += 1
+                        if counter >= results:
+                            raise LoopBreak()
+        except LoopBreak:
+            pass
+    
     return matches
