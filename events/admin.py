ba8b27e60821c9a1895b532fdf867165750762ff
Richard Warburton <richard.warburton@gmail.com>
2009-06-22 11:03:20 +0000

    columns and rows now the correct way round

--- events/admin.py
+++ events/admin.py
@@ -37,8 +37,8 @@ class EventSignupForm(forms.ModelForm):
         seating = data.get('seating')
         if seating:
             e = Event.objects.get(eventsignup__pk=data.get('id'))
-            (rows,cols) = Seating.objects.maximums(e)
-            if rows > seating.max_rows - 1  or cols > seating.max_cols - 1:
+            (cols,rows) = Seating.objects.maximums(e)
+            if rows > seating.max_rows or cols > seating.max_cols:
                 raise forms.ValidationError(u'This seating room is used by is required to be wider or taller than it currently is.')
         
         return data
@@ -66,7 +66,7 @@ class SeatingRoomForm(forms.ModelForm):
         data = self.cleaned_data
         max_cols,max_rows = data.get('max_cols')-1,data.get('max_rows')-1
         for e in Event.objects.filter(eventsignup__seating__pk=data.get('id')):
-            (rows,cols) = Seating.objects.maximums(e)
+            (cols,rows) = Seating.objects.maximums(e)
             if rows > max_rows or cols > max_cols:
                 raise forms.ValidationError(u'This seating room is used by %s, which requires it to be wider or taller than you have set it.'%e)
         return data
