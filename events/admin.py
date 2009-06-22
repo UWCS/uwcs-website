27368a81f382674a136bab43237845e667a71939
Richard Warburton <richard.warburton@gmail.com>
2009-06-22 10:53:14 +0000

    removes signups from event admin interface

--- events/admin.py
+++ events/admin.py
@@ -48,12 +48,12 @@ class EventSignupInline(admin.StackedInline):
     max_num = 1
     form = EventSignupForm
 
-class SignupInline(admin.TabularInline):
-    model = Signup
-    extra = 1
+#class SignupInline(admin.TabularInline):
+#   model = Signup
+#    extra = 1
 
 class EventAdmin(admin.ModelAdmin):
-    inlines = [ EventSignupInline, SignupInline]
+    inlines = [ EventSignupInline ] #, SignupInline]
     form = EventAdminForm
     list_display = ('type', 'location', 'start')
     ordering = ('-start',)
@@ -67,7 +67,7 @@ class SeatingRoomForm(forms.ModelForm):
         max_cols,max_rows = data.get('max_cols')-1,data.get('max_rows')-1
         for e in Event.objects.filter(eventsignup__seating__pk=data.get('id')):
             (rows,cols) = Seating.objects.maximums(e)
-            if rows > max_rows or cols > max_cols:
+            if rows > max_cols or cols > max_rows:
                 raise forms.ValidationError(u'This seating room is used by %s, which requires it to be wider or taller than you have set it.'%e)
         return data
 
