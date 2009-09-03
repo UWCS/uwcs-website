72c2eb505418937548ceb8b73cf10a53114b0775
Tim Monks <t.r.monks@gmail.com>
2009-09-03 11:10:03 +0000

    implemented moving cms pages along with attachments and children

--- cms/models.py
+++ cms/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from urllib import urlopen
 from compsoc.search import register
 from compsoc.settings import GAMING_SERVER, MEDIA_ROOT
+from django.db.models.fields.files import FieldFile
+from django.db import IntegrityError
+
 import os
 
 # set timeout - note this should be written for python 3k
@@ -9,7 +12,7 @@ import socket
 socket.setdefaulttimeout(0.1)
 
 class Page(models.Model):
-    slug = models.CharField(max_length=30)
+    slug = models.CharField(max_length=30, unique=True)
     
     def __unicode__(self):
             return self.slug
@@ -53,22 +56,86 @@ class Page(models.Model):
     def get_absolute_url(self):
         return "/cms/%s" % self.slug
 
+    def move(self, destination, with_attachments=True, with_children=True):
+        """
+        Moves a page to a new location. As well as changing the slug, it
+        has the ability to move attached items and children also. 
+        Will error if the root page cannot be moved, will silently fail to
+        move any subpages that cannot be moved.
+        NOTE: This will always call self.save()
+        """
+        if with_children: children = self.get_children()
+        self.slug = destination
+
+        # unique=True makes sure we can move to here first 
+        self.save()
+
+        # now remember to update the attachments
+        if with_attachments:
+            for a in self.attachment_set.iterator():
+                a.move(self, save=True)
+
+        if with_children:
+            for child in children:
+                suffix = child.slug.rstrip('/').split('/')[-1]
+                # disjointed pages can exist, we might end up moving a 
+                # child to somewhere that already exists, in this case we skip
+                # that one.
+                try:
+                    child.move("%s/%s" % (destination, suffix))
+                except IntegrityError:
+                    pass
+
 register(Page, ['title','text'])
+
+class MovableFieldFile(FieldFile):
+    """
+    An extension to the standard FieldFile with the added ability to be moved/renamed.
+    """
+    def move(self, destination, save=False):
+        """
+        Will move the file on disk and update the model to reflect this.
+        """
+        self._require_file()
+
+        old_location = self._name
+        self._name = self.storage.save(destination, self.file)
+        setattr(self.instance, self.field.name, self.name)
+
+        # Update the filesize cache
+        self._size = len(self.file)
+
+        self.storage.delete(old_location)
+        if save: self.instance.save()
 
 class Attachment(models.Model):
     page = models.ForeignKey(Page)
     title = models.CharField(max_length=50)
 
     def upload_path(self, filename, *args, **kwargs):
+        """
+        Decides where to save the file, based on the page it's
+        attached to.
+        """
         return "cms/%s/attachment/%s" % (self.page.slug, filename)
 
     file = models.FileField(upload_to=upload_path)
+    file.attr_class = MovableFieldFile
 
     def __unicode__(self):
         return self.file.name
 
     def get_absolute_url(self):
         return "/%s" % self.file.name
+
+    def move(self, destination_page, save=False):
+        """
+        Updates the file field and moves physical file to reflect a new page.
+        Useful for updating attachments when you move their related page.
+        """
+        name = self.file.name.rstrip('/').split('/')[-1]
+        self.page = destination_page
+        self.file.move("cms/%s/attachment/%s" % (destination_page.slug, name), save=save)
 
 class PageRevision(models.Model):
     page = models.ForeignKey(Page)
