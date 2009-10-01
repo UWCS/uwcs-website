852d6005e013fdf2f6b8b1335f4d23a71fcc2f0d
Tim Monks <t.r.monks@gmail.com>
2009-10-01 01:56:14 +0000

    new issue view now makes admin logentry

--- tracker/views.py
+++ tracker/views.py
@@ -12,6 +12,10 @@ from django.http import HttpResponseRedirect
 from django.contrib.admin.widgets import AdminSplitDateTime
 from compsoc.memberinfo.forms import UserModelChoiceField
 
+from tracker.models import Ticket
+from django.contrib.admin.models import LogEntry,ADDITION,CHANGE,DELETION
+from django.contrib.contenttypes.models import ContentType
+
 COMPLETED_CHOICES = (
     ('C','Completed'),
     ('O','Open'),
@@ -88,6 +92,14 @@ def new_ticket(request):
             ticket.started = datetime.now()
             ticket.completed = False
             ticket.save()
+            print "logging! %s" % ticket.id
+            LogEntry.objects.log_action(
+                user_id         = request.user.pk,
+                content_type_id = ContentType.objects.get_for_model(ticket).pk,
+                object_id       = ticket.pk,
+                object_repr     = ticket.__unicode__(), 
+                action_flag     = ADDITION,
+            )
             return HttpResponseRedirect('/tickets/')
     else:
         form = TicketForm()
