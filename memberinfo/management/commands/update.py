3bae5c243a67e50b471dc8e1d3fe3355af745c4e
Tim Monks <t.r.monks@gmail.com>
2010-10-06 10:40:55 +0000

    quick hack - don't deactivate society accounts

--- memberinfo/management/commands/update.py
+++ memberinfo/management/commands/update.py
@@ -97,8 +97,8 @@ class Command(NoArgsCommand):
                 # next two lines should be removed when historical exec data is added
                 ex, created = Group.objects.get_or_create(name=EX_EXEC_GROUP_NAME)
 
-                # if not on the exec at some point
-                if not user.groups.filter(name=EX_EXEC_GROUP_NAME):
+                # if not on the exec at some point or a society
+                if not (user.groups.filter(name=EX_EXEC_GROUP_NAME) or Society.objects.filter(user_ptr=u))
                     try:
                         week = warwick_week_for(datetime.now())
                     except Term.DoesNotExist:
