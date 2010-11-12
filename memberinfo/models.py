f7ed595423b0240f49338617353eee43542fa425
Tim Monks <t.r.monks@gmail.com>
2010-11-12 15:18:00 +0000

    new plan, django has built in stuff for stopping signal handlers from being registered multiple times

--- memberinfo/models.py
+++ memberinfo/models.py
@@ -257,3 +257,59 @@ class Society(User):
         verbose_name_plural = "Societies"
 
 post_save.connect(ensure_memberinfo_callback, sender=Society)
+
+def sync_email_with_mailman_database(sender, instance, **kwargs):
+    """
+    Used to ensure that the mailman database stays faithful to
+    the reinhardt one when an email address is updated
+
+    Intended to be triggered pre_save, and act when a user changes
+    their email address
+    """
+    try:
+        old_user = User.objects.get(id=instance.id)
+    except User.DoesNotExist:
+        old_user = instance
+
+    lists = MailingList.objects.filter(users=instance)
+
+    # silently update mailman list subscriptions
+    # not sure what to do on mailman throwing an error here yet
+    for l in lists:
+        try:
+            unsubscribe_member(old_user, l)
+        except MailmanError: pass
+        try:
+            subscribe_member(instance, l)
+        except MailmanError: pass
+
+def mailing_list_users_changed(sender, instance, action, **kwargs):
+    """
+    Acts as a callback when the many2many relation
+    containing users for this list is updated.
+
+    Attempts to commit to mailman before the
+    reinhardt database is updated.
+    """
+    if action == "pre_add":
+        users = User.objects.filter(id__in=kwargs['pk_set'])
+        try:
+            for user in users:
+                subscribe_member(user, instance)
+        # XXX: need to move away from wrapping the different types of
+        # exception all in MailmanError
+        #
+        # preferably here we would check if we can commit all of the users
+        # to the mailman database before trying to
+        #
+        # in the next version. :P -- monk
+        except MailmanError: pass
+    elif action == "pre_remove":
+        users = User.objects.filter(id__in=kwargs['pk_set'])
+        try:
+            for user in users:
+                unsubscribe_member(user, instance)
+        except MailmanError: pass
+
+pre_save.connect(sync_email_with_mailman_database, sender=User, dispatch_uid="sync_email_with_mailman")
+m2m_changed.connect(mailing_list_users_changed, sender=MailingList.users.through, dispatch_uid="mailing_list_users_changed")
