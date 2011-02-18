342f29a6a150d3c35b53b0a7367f1ee69a32409f
Tim Monks <t.r.monks@gmail.com>
2011-02-18 17:07:35 +0000

    javascript code to highlight more 'experienced' members of compsoc in seating plan

--- static/js/seating.js
+++ static/js/seating.js
@@ -348,4 +348,27 @@ var Seating = {
 	}
 }
 
+// this was translated from php and thus probably won't work
+// Thaux
+function hsvBorder(selection,h, s, v){
+    var r, g, b;
 
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+
+    var rgb = "rgb("+(Math.round(r * 255))+", "+(Math.round(g * 255))+", "+(Math.round(b * 255))+")";
+    
+    selection.css({ borderColor:rgb,borderStyle:"solid" });
+}
